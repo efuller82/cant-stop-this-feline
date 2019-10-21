2f9c2d2f9c35654fa25a0e2db350f3a0c4adeb42
Eric Fuller <efuller82@gmail.com>
2019-10-21 17:38:42 -0500

    rumble page added; links working

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -6,7 +6,7 @@ function SideNav() {
         <div>
             <ul className='side-list' id='side-nav'>
                 <li><a href='/addcat' className='list-item'>Add a Cat</a></li>
-                <li><a href='#' className='list-item'>Rumble</a></li>
+                <li><a href='/rumble' className='list-item'>Rumble</a></li>
                 <li><a href='/adopt' className='list-item'>Adopt</a></li>
             </ul>
         </div>
