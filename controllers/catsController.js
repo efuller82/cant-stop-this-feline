e82e6a8a89f2657e06dd38e335c672e9134cc64f
JoJo <jae.kae.tea@gmail.com>
2019-10-29 21:28:27 -0500

    Sorting by upvotes

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -5,7 +5,7 @@ module.exports = {
   findAll: function(req, res) {
     db.Cat
       .find(req.query)
-      .sort({ date: -1 })
+      .sort({ upvotes: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
